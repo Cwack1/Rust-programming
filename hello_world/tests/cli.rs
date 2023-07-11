39c900a345f8a46bf558c5fa4c876773f4f648be
Cwack1 <t92@gmx.com>
2023-07-12 00:06:51 +0200

    Added gitignore

---
+++ hello_world/tests/cli.rs
@@ -0,0 +1,13 @@
+use assert_cmd::Command;
+
+#[test]
+fn runs() {
+    let mut cmd = Command::cargo_bin("hello_world").unwrap();
+    cmd.assert().success();
+}
+
+#[test]
+fn true_ok(){
+    let mut cmd = Command::cargo_bin("true").unwrap();
+    cmd.assert().success();
+}
